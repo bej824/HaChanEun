4e505bb9bd6f6d21a71e46f146cae1976378eede
unknown <bej824@naver.com>
2025-01-24 17:47:03 +0900

    좋아요 구조 변경 및 대댓글 수정시 위에 대댓글이 수정되는 현상 수정

--- Food_Searcher/src/main/java/com/food/searcher/domain/RecipeLikesVO.java
+++ Food_Searcher/src/main/java/com/food/searcher/domain/RecipeLikesVO.java
@@ -15,10 +15,6 @@ public class RecipeLikesVO {
 	private int recipeLikesId;
 	private int recipeBoardId;
 	private String memberId;
-	private int memberAge;
-	private String memberGender;
-	private String memberMBTI;
-	private String memberConstellation;
 	private int memberLike;
 	private int previousMemberLike;
 }
