cf7d9d147ead732942cb5e6a815eac2d6e6b16bb
unknown <bej824@naver.com>
2025-04-03 17:42:39 +0900

    ranking 페이지 수정

--- Food_Searcher/src/main/java/com/food/searcher/controller/RankingController.java
+++ Food_Searcher/src/main/java/com/food/searcher/controller/RankingController.java
@@ -12,6 +12,7 @@ import com.food.searcher.domain.RecipeLikesVO;
 import com.food.searcher.domain.RecipeVO;
 import com.food.searcher.service.RecipeLikesService;
 import com.food.searcher.service.RecipeService;
+import com.food.searcher.service.UtilityService;
 import com.food.searcher.util.Pagination;
 
 import lombok.extern.log4j.Log4j;
@@ -26,15 +27,16 @@ public class RankingController {
 	@Autowired
 	private RecipeService recipeService;
 	
+	@Autowired
+	private UtilityService utilityService;
+	
 	@GetMapping("/ranking")
 	public void main(Model model, Pagination pagination) {
 		log.info(pagination);
 		List<RecipeLikesVO> likeList = recipeLikesService.getPagingBoards(pagination);
-		log.info(likeList);
 		List<RecipeVO> recipeList = new ArrayList<RecipeVO>();
 		for(RecipeLikesVO like : likeList) {
-			RecipeVO recipeVO = recipeService.getBoardById(like.getRecipeId(), like.getMemberId());
-			log.info(recipeVO);
+			RecipeVO recipeVO = recipeService.getBoardById(like.getRecipeId(), utilityService.loginMember());
 			recipeList.add(recipeVO);
 		}
 		model.addAttribute("type", pagination.getType());
